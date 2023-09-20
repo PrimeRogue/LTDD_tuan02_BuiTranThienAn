18273d4835d60f59aed915a153e87dbb0c34bcec
Bui Tran Thien An <buitranthienan1111@gmail.com>
2023-09-20 15:49:27 +0700

    05_XMLHttpRequest_RESTful_DELETE

---
+++ JS/05_XMLHttpRequest_RESTful_DELETE.js
@@ -0,0 +1,12 @@
+var url = "https://62c1218ceff7f7856f0990a7.mockapi.io/shopeelink/link";
+var xhr = new XMLHttpRequest();
+xhr.open("DELETE", url + "/60", true);
+xhr.onload = function () {
+  var users = JSON.parse(xhr.responseText);
+  if (xhr.readyState == 4 && xhr.status == "200") {
+    console.table(users);
+  } else {
+    console.error(users);
+  }
+};
+xhr.send(null);
